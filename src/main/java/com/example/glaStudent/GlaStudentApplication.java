c9f3ce29677a87265b0676b3247651d24123f5d5
aktiwarigla <abhishek.kumar@gla.ac.in>
2023-10-18 22:29:22 +0530

    first

---
+++ src/main/java/com/example/glaStudent/GlaStudentApplication.java
@@ -0,0 +1,13 @@
+package com.example.glaStudent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlaStudentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlaStudentApplication.class, args);
+	}
+
+}
